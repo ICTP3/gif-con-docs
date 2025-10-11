1bc696a21b48920c3ccf6b77f598f755c5a5a926
Yamamoto Ryosei <myros23112006@gmail.com>
2025-10-11 10:24:23 +0900

    ディレクトリの構成を見直し、変更した & viewerの追加

---
+++ viewer/swagger-initializer.js
@@ -0,0 +1,21 @@
+window.onload = function() {
+  //<editor-fold desc="Changeable Configuration Block">
+
+  // the following lines will be replaced by docker/configurator, when it runs in a docker-container
+  window.ui = SwaggerUIBundle({
+    url: "openapi.yaml",
+    dom_id: '#swagger-ui',
+    defaultModelsExpandDepth: -1, // 🔥 Schemasタブを非表示にする設定
+    deepLinking: true,
+    presets: [
+      SwaggerUIBundle.presets.apis,
+      SwaggerUIStandalonePreset
+    ],
+    plugins: [
+      SwaggerUIBundle.plugins.DownloadUrl
+    ],
+    layout: "StandaloneLayout"
+  });
+
+  //</editor-fold>
+};
