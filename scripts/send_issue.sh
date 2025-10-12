6073c5750442449cff71c3787d89cf9ab8fd8ff4
Yamamoto Ryosei <myros23112006@gmail.com>
2025-10-12 22:32:33 +0900

    send_issue.shのエラー部分を解消

--- scripts/send_issue.sh
+++ scripts/send_issue.sh
@@ -33,23 +33,11 @@ payload=$(jq -n \
         "title": $action,
         "color": 5814783,
         "fields": [
-          {
-            "name": "🧾 リポジトリ",
-            "value": "`gif-con-backend`",
-            "inline": true
-          },
-          {
-            "name": "🙋 作成者",
-            "value": "`Yamamoto_Ryosei`",
-            "inline": true
-          },
-          {
-            "name": "📌 Issue",
-            "value": "[#\($num)] \($title)\n🔗 [GitHubで見る](\($url))",
-            "inline": false
-          }
-        ]
-        "footer": { "text": "スタジオジフリ  |  自動通知" },
+          {"name": "🧾 リポジトリ", "value": $repo, "inline": true},
+          {"name": "🙋 作成者", "value": $user, "inline": true},
+          {"name": "📌 Issue", "value": "[#\($num)] \($title)\n\n🔗 [GitHubで見る](\($url))"}
+        ],
+        "footer": { "text": "スタジオジフリ  | 自動通知" },
         "timestamp": (now | todate)
       }
     ]
